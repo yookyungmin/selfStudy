aba4a632952aa9cd3d3c89508c23ec1a990b84b3
yookyungmin <ykmykm4608@naver.com>
2023-02-16 22:11:41 +0900

    프로그래머스 가장 큰수 찾기

--- src/main/Lv0/Exam01.java
+++ src/main/Lv0/Exam01.java
@@ -1,10 +1,39 @@
 package main.Lv0;
+//배열의 유사도
+ class Solution1 {
 
 
+    public int solution(String[] s1, String[] s2) {
+        int answer = 0;
+        int count = 0;
+
+        for(int i = 0; i<s1.length; i++){
+            for(int j = 0; j<s2.length; j++){
+                if(s1[i].equals(s2[j])){
+                    count++;
+                }
+            }
+        }
+        answer = count;
+        return answer;
+    }
+}
 
 public class Exam01 {
     public static void main(String[] args){
-        System.out.println("test");
+        Solution1 s = new Solution1();
+        String[] str1 = {"기린", "펭귄"};
+        String[] str2 = {"기린", "펭귄", "호랑이", "원숭이"};
+
+
+        System.out.println(s.solution(str1,str2));
+
+
+
     }
+
+
+
+
 
 }
