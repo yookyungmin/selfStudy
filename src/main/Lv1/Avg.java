63d93e0dbdaa5d820c2e2e0dc975be0d79bf55fb
yookyungmin <ykmykm4608@naver.com>
2023-02-17 23:58:57 +0900

    프로그래머스 평균 구하기

---
+++ src/main/Lv1/Avg.java
@@ -0,0 +1,35 @@
+package main.Lv1;
+
+
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+
+        double sum = 0;
+
+
+
+        if((arr.length>=1 && arr.length<=100)){
+
+            for(int i = 0; i<arr.length; i++) {
+                sum += arr[i];
+
+                answer = sum/arr.length;
+            }
+        }
+
+
+
+        return answer;
+    }
+}
+
+public class Avg {
+
+    public static void main(String[] args){
+            Solution s = new Solution();
+            int[] arr ={2, 5, 8, 60, 9};
+        System.out.println(s.solution(arr));
+
+    }
+}
