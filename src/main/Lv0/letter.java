f9ff2b6f6eddfbb6f1f413b7448e4927da9f71d9
yookyungmin <ykmykm4608@naver.com>
2023-02-22 23:46:25 +0900

    프로그래머스 편지

---
+++ src/main/Lv0/letter.java
@@ -0,0 +1,20 @@
+package main.Lv0;
+class Solution5 {
+    public int solution(String message) {
+        int answer = 0;
+
+
+
+        return message.length()*2;
+    }
+}
+public class letter {
+
+    public static void main(String[] args){
+
+        Solution5 s = new Solution5();
+
+
+        System.out.println(s.solution("안녕 하세요 "));
+    }
+}
