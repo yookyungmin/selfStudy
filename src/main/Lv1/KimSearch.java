0f91ffa7715c8532780e436845476136832d46df
yookyungmin <ykmykm4608@naver.com>
2023-03-12 15:27:22 +0900

    프로그래머스 서울에서 김서방 찾기

---
+++ src/main/Lv1/KimSearch.java
@@ -0,0 +1,33 @@
+package main.Lv1;
+
+/*
+서울에서 김서방 찾기
+문제 설명
+String형 배열 seoul의 element중 "Kim"의 위치 x를 찾아, "김서방은 x에 있다"는 String을 반환하는 함수, solution을 완성하세요.
+seoul에 "Kim"은 오직 한 번만 나타나며 잘못된 값이 입력되는 경우는 없습니다.
+ */
+class Solution2 {
+    public String solution(String[] seoul) {
+        String answer = "";
+
+
+        for(int i = 0; i < seoul.length; i++){
+            if(seoul[i].equals("Kim")){
+                answer = "김서방은 " + i + "에 있다";
+                break;
+            }
+        }
+
+        return answer;
+
+
+    }
+
+}
+public class KimSearch {
+    public static void main(String[] args) {
+        Solution2 s= new Solution2();
+        String[] str = {"yoo", "park","Kim"};
+        System.out.println(s.solution(str));
+    }
+}
