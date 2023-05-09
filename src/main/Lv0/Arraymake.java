86388411283ee9bdfa44e6f5a51ac877de7a4c34
yookyungmin <ykmykm4608@naver.com>
2023-05-09 19:41:38 +0900

    프로그래머스 배열 만들기

--- src/main/Lv0/Arraymake.java
+++ src/main/Lv0/Arraymake.java
@@ -1,6 +1,12 @@
 package main.Lv0;
 
 import java.util.*;
+
+/*
+배열 만들기 1
+문제 설명
+정수 n과 k가 주어졌을 때, 1 이상 n이하의 정수 중에서 k의 배수를 오름차순으로 저장한 배열을 return 하는 solution 함수를 완성해 주세요.
+ */
 class Solution22 {
     public List solution(int n, int k) {
         List<Integer> answer = new ArrayList<>();
