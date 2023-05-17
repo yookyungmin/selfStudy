c566b646c7730a42339d1b5eee1260a4d12c1cad
yookyungmin <ykmykm4608@naver.com>
2023-05-17 12:13:53 +0900

    백주 카드2 Queue

---
+++ src/main/Gold/Good.java
@@ -0,0 +1,24 @@
+//package main.Gold;
+//
+//class Solution1{
+//    public int solution(int[] a){
+//
+//        int count = 0;
+//
+//        for(int i = 0; i<a.length; i++){
+//            if(a[i]+a[i+1]){
+//                count++;
+//            }
+//        }
+//
+//
+//        return
+//    }
+//}
+//
+//public class Good {
+//
+//    public static void main(String[] args) {
+//        Solution1 s = new Solution1();
+//    }
+//}
