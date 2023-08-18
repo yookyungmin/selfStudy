bc27bda1dbde2b14acb19de7eba7307d6360c748
yookyungmin <ykmykm4608@naver.com>
2023-08-18 17:55:43 +0900

    프로그래머스 Lv1 약수의 합

---
+++ src/main/Lv1/MeasurePlus.java
@@ -0,0 +1,21 @@
+package main.Lv1;
+
+class Solution48{
+    public int solution(int n){
+        int answer = 0;
+        for(int i = 1; i<=n; i++){
+            if(n%i == 0){
+                answer += i;
+            }
+        }
+    }
+}
+public class MeasurePlus {
+    public static void main(String[] args) {
+        Solution48 s = new Solution48();
+
+        int number = 12;
+
+        System.out.println(s.solution(number));
+    }
+}
