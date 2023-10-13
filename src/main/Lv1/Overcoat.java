fc0bee3970facb0c85b62ad06d3f6647a76f4af4
yookyungmin <ykmykm4608@naver.com>
2023-10-14 00:28:34 +0900

    프로그래머스 Lv2 덧칠

--- src/main/Lv1/Overcoat.java
+++ src/main/Lv1/Overcoat.java
@@ -2,6 +2,14 @@ package main.Lv1;
 
 import java.sql.SQLOutput;
 
+/*
+해당 문제는 그리디 알고리즘을 사용하는게 적합하다고 판단했다.
+
+페인트 칠이 필요한 벽들의 번호를 담은 배열을 순서대로 탐색하여 처음 벽 번호 + 롤러 크기만큼에
+해당하는 벽은 한번 칠한 걸로 하고 계속 반복하면 된다.
+
+ 현재 상황에서 가장 좋은 것(최선의 선택)을 고르는 알고리즘을 말합니다.
+ */
 class Solution56 {
     public int solution(int n, int m, int[] section) {
         int answer = 0;
