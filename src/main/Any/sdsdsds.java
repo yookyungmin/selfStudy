e262bde7ebfc1d0672c6fab8c1ca3727a143e88d
yookyungmin <ykmykm4608@naver.com>
2025-03-27 14:50:47 +0900

    git 히스토리 복구 테스트v333333333333333333

--- src/main/Any/sdsdsds.java
+++ src/main/Any/sdsdsds.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class StreamAssi {
+public class sdsdsds {
     public static void main(String[] args) {
         //list 생성
         List<String> list = Arrays.asList("apple", "banna", "cherry", "date", "elderberry");
